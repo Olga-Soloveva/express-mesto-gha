05e396d868f752c52faae5c3e1a9999f53db3000
Olga Tovstaya <olga.tovstaya@yandex.ru>
2022-09-24 02:11:10 +0300

    feat: реализовать другие роуты карточек и пользователя

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,7 +2,7 @@ const Card = require("../models/card");
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .populate('owner')
+    .populate(["owner", "likes"])
     .then((cards) => res.send({ data: cards }))
     .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 };
@@ -11,12 +11,33 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then(card => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+};
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 };
+
+module.exports.likeCard = (req, res) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .populate(["owner", "likes"])
+
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+
+module.exports.dislikeCard = (req, res) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .populate(["owner", "likes"])
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
--- controllers/users.js
+++ controllers/users.js
@@ -16,6 +16,40 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then(user => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name: name, about: about },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    }
+  )
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: avatar },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    }
+  )
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+
