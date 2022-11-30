6952e8f087c9e4a1e23f3dec2c6b93d759c3ee8c
Olga Tovstaya <olga.tovstaya@yandex.ru>
2022-11-30 22:16:40 +0300

    hotfix: исправить cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -13,9 +13,11 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
 
     const { method } = req;
+    const requestHeaders = req.headers['access-control-request-headers'];
 
     if (method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
     }
     return res.end();
   }
