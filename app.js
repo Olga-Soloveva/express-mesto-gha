da88cb1facde44988682983f1d5b783760b8b971
Olga Tovstaya <olga.tovstaya@yandex.ru>
2022-12-02 01:50:34 +0300

    hotfix: перенести cors в app.js

--- app.js
+++ app.js
@@ -13,14 +13,39 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { notFoundController } = require('./controllers/notFoundController');
 const auth = require('./middlewares/auth');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 const { REGEX, SERVER_ERROR_CODE } = require('./utils/constants');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors);
+// app.use(cors);
+const allowedCors = [
+  'https://olgatovstaya.mesto.nomoredomains.club',
+  'http://olgatovstaya.mesto.nomoredomains.club',
+  'http://localhost:3000',
+];
+
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.use(cookieParser());
 
