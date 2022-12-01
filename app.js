2dfc47783604e007490f72358df63757320c8f16
Olga Tovstaya <olga.tovstaya@yandex.ru>
2022-12-01 11:43:25 +0300

    hotfix: исправить cors на *

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { celebrate, errors, Joi } = require('celebrate');
-// const cors = require('cors');
+const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { login, createUser } = require('./controllers/users');
@@ -14,7 +14,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { notFoundController } = require('./controllers/notFoundController');
 const auth = require('./middlewares/auth');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 const { REGEX, SERVER_ERROR_CODE } = require('./utils/constants');
 
 const { PORT = 3000 } = process.env;
@@ -31,38 +31,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(requestLogger);
 
-// const allowedCors = [
-//   'https://olgatovstaya.mesto.nomoredomains.club',
-//   'http://olgatovstaya.mesto.nomoredomains.club',
-//   'localhost:3000',
-// ];
-
-// const corsOptions = {
-//   origin(origin, callback) {
-//     if (allowedCors.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
-
-// app.use(cors(corsOptions));
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-// eslint-disable-next-line max-len
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-//   if (req.method === 'OPTIONS') {
-//     res.send(200);
-//   } else {
-//     next();
-//   }
-// });
-
-app.use(cors);
+app.use(cors({
+  origin: '*',
+}));
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
