31a9497bfda2fbb591a9bd2a654676a2c4b92ea8
Olga Tovstaya <olga.tovstaya@yandex.ru>
2022-12-01 10:26:43 +0300

    hotfix: реализовать cors через мидлвер

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { celebrate, errors, Joi } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { login, createUser } = require('./controllers/users');
@@ -14,7 +14,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { notFoundController } = require('./controllers/notFoundController');
 const auth = require('./middlewares/auth');
-// const cors = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 const { REGEX, SERVER_ERROR_CODE } = require('./utils/constants');
 
 const { PORT = 3000 } = process.env;
@@ -22,7 +22,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -62,7 +62,7 @@ app.use(requestLogger);
 //   }
 // });
 
-// app.use(cors);
+app.use(cors);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,15 +11,16 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
 
-    const { method } = req;
-    const requestHeaders = req.headers['access-control-request-headers'];
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
-    }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
   }
   next();
 };
